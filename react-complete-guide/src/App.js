dbd22eac25115c38a929a304981378c698cde9cb
David Huertas <dhuertas.eng@gmail.com>
2021-04-01 02:46:27 +0200

    Module 4: Flexible lists

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,14 +13,24 @@ class App extends Component {
     showPersons: false
   };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'David', age: 33},
-        { name: event.target.value, age: 31},
-        { name: 'Eris', age: 3}
-      ]
-    } )
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //an alternative: const person = Object.assign({}, this.state.persons[personIndex]):
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    this.setState( {persons: persons} );
   }
 
   deletePersonHandler = (personIndex) => {
@@ -55,6 +65,7 @@ class App extends Component {
                   name={person.name}
                   age={person.age} 
                   key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}
                   />
               })}
             </div>
